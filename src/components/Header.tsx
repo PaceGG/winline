e55ce3899b07333674a43ba032262e34f3675cdd
PaceGG <yura3012004@gmail.com>
2025-11-26 18:29:32 +0500

    feat: добавлен компонент логотипа

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import { AppBar, Box } from "@mui/material";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <AppBar>
+      <Logo />
       <Box>Header</Box>
     </AppBar>
   );
---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@mui/material";
+
+export default function Logo() {
+  return <Box component="img" src="logo.svg" alt="Winline" height={40} />;
+}
